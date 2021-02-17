ef104e31ea88ea821fc742c485a65dd893d5eeac
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2021-02-16 22:13:55 -0300

    [+] New Activities

---
+++ src/main/kotlin/exercicies/functions/06-NewActivites.kt
@@ -0,0 +1,22 @@
+package exercicies.functions
+/*Crie um programa que sugira uma atividade baseada em vários parâmetros.
+
+Comece em um novo arquivo com uma função main.
+A partir de main(), crie uma função whatShouldIDoToday().
+Deixe a função com três parâmetros.
+mood: um parâmetro de string obrigatório
+weather: um parâmetro de string cujo padrão é "sunny"
+temperature: um parâmetro Integer padronizado para 24 graus (Celsius).
+Use um construtor when para retornar algumas atividades usando como base combinações de condições.*/
+
+fun whatShouldIDoToday(mood: String, weather: String = "sunny", temperature: Int = 24): String {
+    return when {
+        temperature == 21 -> "This is a good temperature for fish. Try to keep it."
+        mood == "happy" && weather == "Sunny" -> "go for a walk"
+        else -> "Stay home and read."
+    }
+}
+
+fun main() {
+    println(whatShouldIDoToday("sad"))
+}
